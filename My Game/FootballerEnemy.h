a7e7cd8e782a0fc934dc094785c2d8b6c59fd1e9
Jackson Johanek <jack.johanek@gmail.com>
2022-12-05 13:32:26 -0600

    Created final enemy types

---
+++ My Game/FootballerEnemy.h
@@ -0,0 +1,31 @@
+/// \file FootballerEnemy.h
+/// \brief Interface for the FootballerEnemy object class FootballerEnemy.
+
+#ifndef __L4RC_GAME_FootballerEnemy_H__
+#define __L4RC_GAME_FootballerEnemy_H__
+
+#include "RangedWeapon.h"	//TODO: Change for meleeweapon
+#include "Enemy.h"
+
+/// \brief The FootballerEnemy object. 
+///
+/// FootballerEnemy is the abstract representation of a FootballerEnemy object.
+
+class CFootballerEnemy : public CEnemy {
+protected:
+	void RotateTowardsAndShootInRange(const Vector2&); ///< Swivel towards position.
+	void RotateTowardsAndMove(const Vector2&);  ///< Move towards player
+
+	float BuffCooldown = 10.0f;
+
+public:
+	CRangedWeapon* weapon;
+
+	CFootballerEnemy(const Vector2& p); ///< Constructor.
+	~CFootballerEnemy();///Deconstructor
+
+	void CheerBuff(std::list<CObject*>);
+	void move() override; ///< Move FootballerEnemy
+}; //CBullet
+
+#endif //__L4RC_GAME_FootballerEnemy_H__
